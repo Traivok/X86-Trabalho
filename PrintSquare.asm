e57dcf5d65f130e84199b467b24fa07cd2a684b5
Traivok <jraf@cin.ufpe.br>
2017-09-12 20:05:24 -0300

    Fixed jump bug, jl -> jmp

--- PrintSquare.asm
+++ PrintSquare.asm
@@ -6,17 +6,26 @@ start:
 	mov ax, 0
 	mov ds, ax
 	mov es, ax
+	mov ss, ax		; stack init
+	mov sp, 0x7c00		; stack init	
 
-	mov ah, 0
-	mov al, 12h
-	int 10h
+	; mov ah, 0
+	; mov al, 12h
+	; int 10h
 
-	mov ah, 0xb
-	mov bh, 0
-	mov bl, 15
-	int 10h
+	; mov ah, 0xb
+	; mov bh, 0
+	; mov bl, 15
+	; int 10h
 
-
+	mov al, 0xf
+	mov cx, 0
+	mov bx, 512
+	mov dx, 256
+	call printLine
+	
+	jmp done
+	
 ;;; Print a line of pixels
 ;; @param: al will be the color
 ;; @param: cx the start X value, bx the end X value
@@ -29,26 +38,37 @@ printLine:
 
 	cmp cx, bx		; check if the cx has been exceeded	
 	jae .nextRow		; if it it's, using a unsigned ge comparison, then compute next row
-	jl .loop		; if not, print the next coordinate
+	jmp .loop		; if not, print the next coordinate
 	
 	push bx			; bx will be used at next interruption, so store it's value
-	
+
+	mov al, 0xf
 	mov ah, 0ch		; Write graphics pixel -> AL = Color, BH = Page Number, CX = x, DX = y
 	mov bh, 0
 	int 10h			; call graphical interrupt
 	
 	pop bx			; restore bx value
 	inc cx			; compute next coordinate
-	
-	
+		
 .nextRow:			; increment Y axis and set X axis to start value
 	inc dx			; if you want to print more than one row in the screen, dx will be the next coordinate
 	pop cx			; and the cx, the initial value of x axis was stored
-	jl .end
-
+	jmp .end
+	
 .end:
 	ret
-	
+
+;;; Print rectangle
+;; @param: push onto stack in that order:
+;; Color -> Begin X coordinate -> Begin Y coordinate -> End X coordinate -> End Y coordinate
+;; @reg: ax, bx, dx, cx
+printRect:
+	; pop al
+	pop dx
+	pop cx
+	; pop 
+
+done:
 jmp $
 times 510-($-$$) db 0
 dw 0xaa55
