61ae6965e021f50afdd3f1a0d5c37fbd67852c8f
Gabriela Correia Holanda Freitas <gchf@cin.ufpe.br>
2017-09-14 19:56:12 -0300

    Add files via upload

--- loading.asm
+++ loading.asm
@@ -1,23 +1,17 @@
 org 0x7c00
 jmp 0x0000:start
 
-initX dw 90
-initY dw 200
-endX dw 550
-endY dw 270
-color db 7
-
 str1 db "Loading structures for the kernel...", 13, 10, 0
 str2 db "Setting up protected mode...", 13, 10, 0
 str3 db "Loading kernel in memory...", 13, 10, 0
 str4 db "Loading a lot of other things...", 13, 10, 0
-str5 db "Loading reasons to live...", 13, 10, 0
+str5 db "Loading reasons to live... ERROR", 13, 10, 0
 str6 db "Shovelling coal into the server...", 13, 10, 0
 str7 db "Feel free to wait forever...", 13, 10, 0
 str8 db "Spinning up the hamster...", 13, 10, 0
 str9 db "It is a little tired today...", 13, 10, 0
-str9 db "Are you still here?", 13, 10, 0
-str10 db "Okay, lets go..."
+str10 db "Are you still here?", 13, 10, 0
+str11 db "Okay, lets go...", 13, 10, 0
 
 start:
 	mov ax, 0
@@ -29,12 +23,83 @@ start:
 	mov ah, 0
 	mov al, 12h
 	int 10h
-	mov ah, 0xb
-	mov bh, 0
-	mov bl, 4
-	int 10h
+	; mov ah, 0xb
+	; mov bh, 0
+	; mov bl, 4
+	; int 10h
 
+	mov si, str1
+	call printstr
+	call delay
+
+	mov si, str2
+	call printstr
+	call delay
+
+	mov si, str3
+	call printstr
+	call delay
+
+	mov si, str4
+	call printstr
+	call delay
+
+	mov si, str5
+	call printstr
+	call delay
+
+	mov si, str6
+	call printstr
+	call delay
+
+	mov si, str7
+	call printstr
+	call delay
+
+	mov si, str8
+	call printstr
+	call delay
+
+	mov si, str9
+	call printstr
+	call delay
+
+	mov si, str10
+	call printstr
+	call delay
+
+	mov si, str11
+	call printstr
+	call delay
 	jmp done
+
+;; 1 second delay
+delay:
+	mov cx, 0fh
+	mov dx, 4240h
+	mov ah, 86h
+	int 15h
+
+;;; print string
+;; @param: use si to print
+;; @reg: ax, bx
+printstr:
+	.start:
+
+		lodsb 		; si -> al
+		cmp al, 0
+		je .done 	; if (end of string) return
+		jmp .print 	; else print current char
+
+		.print:
+			mov ah, 0xe 	; print char and move cursor foward
+			mov bh, 0 	; page number
+			mov bl, 0xf 	; white color
+			int 10h 	; video interrupt
+
+			jmp .start 
+		.done:
+			ret
 
 done:
 	jmp $
