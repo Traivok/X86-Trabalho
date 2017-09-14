cad1eefc3e1414de2f83975e5018faf880b2022d
Gabriela Correia Holanda Freitas <gchf@cin.ufpe.br>
2017-09-14 14:14:43 -0300

    Add files via upload

---
+++ rect.asm
@@ -0,0 +1,155 @@
+org 0x7c00
+jmp 0x0000:start
+
+initX dw 5
+initY dw 0
+endX dw 200
+endY dw 250
+color db 0xb
+
+start:
+	mov ax, 0
+	mov ds, ax
+	mov es, ax
+	mov ss, ax		; stack init
+	mov sp, 0x7c00		; stack init
+
+	mov ah, 0
+	mov al, 12h
+	int 10h
+	mov ah, 0xb
+	mov bh, 0
+	mov bl, 15
+	int 10h
+
+	call printRect
+
+.loop:
+	call VerticalRand
+	call HorizontalRand
+
+	mov cx, 15
+	xor bx, bx ; bx = 0
+	call randint ; return dx as the random int
+	mov byte [color], dl
+
+	call printRect
+
+	jmp .loop
+
+	jmp done
+
+VerticalRand:
+	mov cx, 200
+	xor bx, bx ; bx = 0
+	call randint ; return dx as the random int
+	mov word [initY], dx
+
+	mov cx, 280
+	mov bx, 200
+	call randint ; return dx as the random int
+	mov word [endY], dx
+.done:
+	ret
+
+HorizontalRand:
+
+	mov cx, 300
+	xor bx, bx ; bx = 0
+	call randint ; return dx as the random int
+	mov word [initX], dx
+
+	mov cx, 340
+	mov bx, 300 ; bx = 0
+	call randint ; return dx as the random int
+	mov word [endX], dx
+
+.done:
+	ret
+
+;;; random number within a range (a:b)
+;; @param: cx contains the absolute interval (b - a)
+;; @param: bx contains the base (a)
+;; @ret: dx, the random number
+;; @reg: ax, bx, dx, cx
+randint:
+	.start:
+		push bx
+		push cx
+	.timeInterrupt:
+		xor ax, ax
+		mov ah, 00h			; interrupts to get system time
+		int 1ah				; CX:DX now hold number of clock ticks since midnight
+	.processInterval:
+		mov ax, dx
+		xor dx, dx
+		
+		pop cx
+		div cx
+
+		pop bx
+		add dx, bx
+
+	.end:
+		ret
+
+
+;;; Print a column of pixels
+;; @param: al will be the color
+;; @param: dx the start Y value, bx the end Y value
+;; @param: cx the X value
+;; @ret: cx will be the next X value
+;; @red: ax, cx, dx, bx
+printColumn:
+	push dx			; stores the initial value of Y
+
+.loop:				; print loop
+	cmp dx, bx		; check if the dx has been exceeded	
+	jae .end		; if it it's, using a unsigned ge comparison, then compute next row
+	
+	push bx			; bx will be used at next interruption, so store it's value
+
+	mov ah, 0ch		; Write graphics pixel -> AL = Color, BH = Page Number, CX = x, DX = y
+	mov bh, 0
+	int 10h			; call graphical interrupt
+	
+	pop bx			; restore bx value
+	inc dx			; compute next coordinate
+
+	jmp .loop		; do the loop again, printing next (x, y + 1) pixel
+		
+.end:				; increment X axis and set Y axis to start value
+	inc cx			; if you want to print more than one row in the screen, cx will be the next coordinate
+	pop dx			; and the dx, the initial value of y axis was stored
+	ret
+
+;;; Print a rectangle
+;; @param: Push the parameters onto stack
+;; 1st push the end X
+;; 2nd push the start X 
+;; 3rd push end Y
+;; 4th push initial Y
+;; 5th push the color
+;; @reg: ax, cx, bx, dx it also use a byte
+printRect:
+.begin:
+	mov dx, word [initY]
+	mov bx, word [endY]
+	mov cx, word [initX]
+.loop:
+	mov ax, word [endX]
+	cmp cx, ax 
+	jae .end
+
+	mov al, byte [color]
+	call printColumn
+
+	jmp .loop
+	
+.end:
+	ret
+
+done:
+	jmp $
+	times 510-($-$$) db 0
+	dw 0xaa55
